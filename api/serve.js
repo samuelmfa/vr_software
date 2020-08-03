6c5934f6e8cc4404e37ae4a472fbf5a162113b6b
Samuel Pereira Costa <46694833+samuelmfa@users.noreply.github.com>
2020-08-03 16:16:53 -0300

    construção da api node

---
+++ api/serve.js
@@ -0,0 +1,42 @@
+'use strict'
+
+const http = require('http');
+const debug = require('debug')('nodestr:server');
+const express = require('express');
+
+const app = express();
+const port = normalizePort(process.env.PORT || '3000');
+app.set('port', port);
+
+const server = http.createServer(app);
+const router = express.Router();
+
+const route = router.get('/', (req, resp, next) =>{
+  resp.status(200).send({
+    title: "Node Store API",
+    version: "0.0.1"
+  });
+});
+
+app.use('/', route);
+
+server.listen(port);
+
+console.log(`API Rodando na porta: ${port}`);
+
+function normalizePort(val) {
+  const port = parseInt(val, 10);
+  if(isNaN(port)){
+    return val;
+  }
+  if(port >= 0) {
+    return port;
+  }
+  return false;
+}
+
+
+
+
+
+
